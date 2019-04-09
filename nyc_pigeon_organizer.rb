362367ec6cbe7a73b9481e5bef0b7da5587faa38
John Hill <john.m.hill91@gmail.com>
2019-04-09 22:55:35 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,5 +2,6 @@ def nyc_pigeon_organizer(data)
   pidgeonByName={}
   data.each do |data, hash|
     hash.each do |details, names|
-      names.each do 
+      names.each do |name|
+        if pidgeonByName[name] == 
 end
